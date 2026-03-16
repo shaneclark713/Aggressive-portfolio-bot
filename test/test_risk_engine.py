0cf8d88fb8b98373a4ddfb9ba0a158af323d24f3
shaneclark713 <shaneclark713@gmail.com>
2026-03-16 01:02:22 -0700

    Delete test directory

--- test/test_risk_engine.py
+++
@@ -1,5 +0,0 @@
-from risk.risk_engine import RiskEngine
-
-def test_risk_engine_has_result():
-    data=RiskEngine().calculate_trade_parameters('SPY', 100, 'LONG', 2, 110, 90)
-    assert 'is_valid' in data

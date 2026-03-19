1efd99a23eacf11cf43398b3907ce3039282a18a
shaneclark713 <shaneclark713@gmail.com>
2026-03-18 19:32:56 -0700

    Add fundamental filters configuration

---
+++ config/filters/fundamental.py
@@ -0,0 +1,6 @@
+FUNDAMENTAL_FILTERS = {
+    "exclude_upcoming_earnings_within_days": 3,
+    "require_positive_revenue_growth": False,
+    "require_positive_eps_growth": False,
+    "max_short_float_pct": 25.0,
+}

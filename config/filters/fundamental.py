d5985d84bb6d493bd609ff7ab70e824a4df4b833
shaneclark713 <shaneclark713@gmail.com>
2026-03-19 04:59:27 -0700

    Add files via upload

--- config/filters/fundamental.py
+++ config/filters/fundamental.py
@@ -1,6 +1,12 @@
 FUNDAMENTAL_FILTERS = {
     "exclude_upcoming_earnings_within_days": 3,
+    "exclude_recent_earnings_within_days": 1,
+    "block_fed_event_window_hours": 6,
     "require_positive_revenue_growth": False,
     "require_positive_eps_growth": False,
     "max_short_float_pct": 25.0,
+    "min_insider_ownership_pct": 0.0,
+    "min_institutional_ownership_pct": 0.0,
+    "minimum_analyst_coverage": 0,
+    "require_news_catalyst": False,
 }
--- config/filters/technical.py
+++ config/filters/technical.py
@@ -1,6 +1,13 @@
 TECHNICAL_FILTERS = {
     "atr_min_pct": 0.02,
+    "atr_max_pct": 0.20,
     "adx_min": 20,
     "relative_strength_lookback_minutes": 30,
     "volume_vs_average_min_ratio": 1.5,
+    "premarket_gap_max_pct": 15.0,
+    "max_extension_from_ema9_pct": 4.0,
+    "require_vwap_alignment": True,
+    "breakout_buffer_pct": 0.10,
+    "opening_range_break_minutes": 15,
+    "minimum_rr_ratio": 2.0,
 }

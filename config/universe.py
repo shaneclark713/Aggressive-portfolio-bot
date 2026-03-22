dfca89147f1be923f3fd5faf2be2cc2372674a5a
shaneclark713 <shaneclark713@gmail.com>
2026-03-22 02:18:08 -0700

    Add files via upload

--- config/universe.py
+++ config/universe.py
@@ -1,5 +1,22 @@
-CORE_EQUITIES = ['SPY','QQQ','IWM','TSLA','NVDA','AMD','AAPL','META','MSFT','AMZN']
+CORE_EQUITIES = [
+    'SPY','QQQ','IWM','DIA',
+    'TSLA','NVDA','AMD','AAPL','META','MSFT','AMZN','GOOGL','NFLX','AVGO','PLTR',
+    'SMCI','MU','INTC','ARM','TSM','BABA','COIN','MSTR','SOFI','HOOD','RIVN','NIO',
+    'XOM','CVX','XLE','JPM','BAC','GS','XLF','UNH','LLY','XBI','XLV'
+]
 CORE_FUTURES = ['MES','MNQ','MYM','M2K']
 EXCLUDED_TICKERS = []
-DAY_TRADE_SETTINGS = {'min_price':20.0,'min_avg_daily_volume':3_000_000,'min_relative_volume':1.5,'min_atr_pct':0.02,'max_gap_pct':0.15}
-SWING_TRADE_SETTINGS = {'min_price':15.0,'min_avg_daily_volume':1_000_000,'min_relative_volume':1.0,'min_atr_pct':0.015,'max_gap_pct':0.10}
+DAY_TRADE_SETTINGS = {
+    'min_price': 10.0,
+    'min_avg_daily_volume': 1_500_000,
+    'min_relative_volume': 1.1,
+    'min_atr_pct': 0.015,
+    'max_gap_pct': 0.20,
+}
+SWING_TRADE_SETTINGS = {
+    'min_price': 10.0,
+    'min_avg_daily_volume': 1_000_000,
+    'min_relative_volume': 1.0,
+    'min_atr_pct': 0.012,
+    'max_gap_pct': 0.15,
+}

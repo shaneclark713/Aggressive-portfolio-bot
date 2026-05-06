f8ceab879aefb400170833f9176cb6fcaf4ac963
shaneclark713 <shaneclark713@gmail.com>
2026-05-05 17:22:38 -0700

    Delete tests directory

--- tests/test_scheduler.py
+++
@@ -1,3 +0,0 @@
-from core.scheduler import build_scheduler
-
-def test_build_scheduler(): assert build_scheduler('America/Phoenix') is not None
--- tests/test_strategy_router.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from strategies.router import StrategyRouter
-
-def test_router_callable():
-    df=pd.DataFrame({'open':[1]*60,'high':[1.1]*60,'low':[0.9]*60,'close':[1]*60,'volume':[1000]*60})
-    StrategyRouter().evaluate_ticker('SPY', df)
